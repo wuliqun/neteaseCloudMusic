fbbcc55e9c7372773b8259a290f4148b822dc705
wuliqun <540744846@qq.com>
2019-06-26 18:03:23 +0800

    search page

---
+++ src/pages/search/components/searchTab.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="search-tab clearfix">
+        <div 
+            class="tab-item"
+            :style="{width:tabWidth}"
+            v-for="tab in tabInfo"
+            :class="{active:type===tab.type}"
+            :key="tab.type">
+            <div      
+                @click="changeType(tab.type)"           
+                class="tab-link">
+                {{ tab.name }}
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import tabInfo from './searchTabData'
+    console.log(tabInfo)
+    export default {
+        data(){
+            return {
+                tabInfo:tabInfo
+            }
+        },
+        props:{
+            type:{
+                type:Number,
+                default:1
+            }
+        },
+        methods:{
+            changeType(type){
+                this.$emit('changetype',type);
+            }
+        },
+        computed:{
+            tabWidth(){
+                return (1/this.tabInfo.length)*100 + '%';
+            }
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    .search-tab{
+        border:solid #ccc;
+        border-width:0 1px;
+        .tab-item{
+            float: left;
+            box-sizing: border-box;
+            border-top:2px solid #ccc;            
+            border-bottom:1px solid #ccc;
+            line-height: 36px;
+            color:#333;
+            text-align: center;
+            transition:all .5s;
+            &.active{
+                border-top-color:#d13030;
+                border-bottom-color:transparent;
+                .tab-link{
+                    border-color:transparent #ccc;
+                }
+            }
+            &:first-child{
+                .tab-link{
+                    border-left: none;
+                }                
+            }
+            &:last-child{
+                .tab-link{
+                    border-right: none;
+                }                
+            }
+            &:hover{
+                border-top-color:#d13030;
+            }            
+        }
+        .tab-link{
+            border:solid transparent;
+            border-width:0 1px;
+            cursor:pointer;
+        }
+    }
+</style>
