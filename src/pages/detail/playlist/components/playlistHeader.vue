bde21e01e267013cd71a256dcedfb2883754828a
wuliqun <540744846@qq.com>
2019-06-28 15:23:00 +0800

    playlist detail page

---
+++ src/pages/detail/playlist/components/playlistHeader.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="playlist-head">
+        <div class="title-wrapper clearfix">
+            <div class="tag-wrapper">
+                <detail-tag type="playlist"></detail-tag>
+            </div>
+            <h2 class="title">{{ playlist.name }}</h2>
+        </div>
+    </div>
+</template>
+<script>
+    import {
+			CollectButton,
+			CommentButton,
+			PlayAddButton,
+			DownloadButton,
+			ShareButton
+        }from '@components/buttons'
+    import DetailTag from '@components/detailTag'
+    export default {
+        props:{
+            playlist:Object
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    .song-head{
+        .title-wrapper{
+            margin-bottom: 15px;
+            .tag-wrapper{
+                float: left;
+            }
+            .title{
+                float: left;
+                margin-left: 10px;
+                font-size: 24px;
+                line-height: 24px;
+                color:#333;
+            }
+        }
+    }
+</style>
--- src/pages/detail/playlist/detail.vue
+++ src/pages/detail/playlist/detail.vue
@@ -1,5 +1,70 @@
+<template>
+    <div class="playlist-detail detail-content">
+        <div class="detail-wrapper clearfix">
+            <div class="detail-left" v-if="playlist">
+                <div class="head clearfix">
+                    <div class="img">
+                        <img :src="playlist.coverImgUrl" alt="">
+                    </div>
+                    <div class="head-wrap">
+                        <playlist-header
+                            :playlist="playlist"></playlist-header>
+                    </div>
+                </div>
+                <div class="comment-wrapper" v-if="song">
+                    <comment
+                        type="playlist"
+                        :id="playlist.id"></comment>
+                </div>    
+            </div>
+            <div class="detail-right">
+                <div class="app-wrapper">
+                    <app-download></app-download>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
 <script>
+import { getPlaylistDetail } from '@service/getData'
+import PlaylistHeader from './components/playlistHeader'
 export default {
-    
+    data(){
+        return {
+            playlist:null,
+            id:null
+        }
+    },
+    methods:{
+        initData(){
+            var id = this.id;
+            getPlaylistDetail(id).then(res=>{
+                this.playlist = res.data.playlist;
+            });
+        }
+    },
+    watch:{
+        '$route.query.id'(id){
+            if(id !== this.id){
+                this.id = id;
+                this.initData();
+            }
+        }
+    },
+    created(){
+        var id = this.$route.query.id;
+        if(!id){
+            this.$router.redirectTo({
+                name:'index'
+            })
+        }else{
+            this.id = id;
+            this.initData();
+        }
+    },
+    components:{
+        PlaylistHeader
+    }
 }
 </script>
